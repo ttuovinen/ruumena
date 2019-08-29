0ff01f43e3b3ad262ac09bea2911f6a2300467e4
Teemu Tuovinen <teetuo@Teemus-MacBook-Pro.local>
2019-08-29 09:21:40 +0300

    Make some nitty-gritty tweaks to sorters code

--- src/tools/sorters.js
+++ src/tools/sorters.js
@@ -1,5 +1,5 @@
 /* Function for creating sorter functions */
-const CreateSorter = (preProcess) => (a, b) => {
+const createSorter = preProcess => (a, b) => {
   const [a2, b2] = [a, b].map(i => (
     preProcess(i)
   ))
@@ -7,16 +7,16 @@ const CreateSorter = (preProcess) => (a, b) => {
 }
 
 /* Let's create a bunch of them */
-const byLength = CreateSorter(i => i.length)
-const fromStart = CreateSorter(i => i.toLowerCase())
-const fromEnd = CreateSorter(i => [...i.toLowerCase()].reverse().join(''))
-const shuffle = CreateSorter(i => Math.random() - 0.5)
+const byLength = createSorter(i => i.length)
+const fromStart = createSorter(i => i.toLowerCase())
+const fromEnd = createSorter(i => [...i.toLowerCase()].reverse().join(''))
+const shuffle = createSorter(i => Math.random() - 0.5)
 
 /* A nice international regex for detecting non-space-or-alphanumeric characters */
 const specialChars = /[^ a-zA-ZáàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ0-9]/g
 
 /* Function for creating wordlist functions */
-const CreateWordLister = (sorter) => ({ seed, reverse, noDuplicates }) => {
+const createWordLister = sorter => ({ seed, reverse, noDuplicates }) => {
   let words = seed
     .replace(/\s+/g, ' ')
     .replace(specialChars, '')
@@ -24,13 +24,13 @@ const CreateWordLister = (sorter) => ({ seed, reverse, noDuplicates }) => {
     .split(' ')
     .filter(word => word) // remove empty strings
     .sort(sorter)
-  if (reverse) { words = words.reverse() }
   if (noDuplicates) { words = [...new Set(words)] }
+  if (reverse) { words = words.reverse() }
   return words.join(' ')
 }
 
 /* Let's create also a bunch of word listers with our sorters and export them */
-export const lengthsortWords = CreateWordLister(byLength)
-export const alphasortWords = CreateWordLister(fromStart)
-export const alphasortWordsFromEnd = CreateWordLister(fromEnd)
-export const shuffleWords = CreateWordLister(shuffle)
+export const lengthsortWords = createWordLister(byLength)
+export const alphasortWords = createWordLister(fromStart)
+export const alphasortWordsFromEnd = createWordLister(fromEnd)
+export const shuffleWords = createWordLister(shuffle)
