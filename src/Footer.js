7a23fe09bc95f2a129697428beb2f0b52d49dd0f
Teemu Tuovinen <teetuo@dyn103-119.agora.net.utu.fi>
2019-08-30 10:32:45 +0300

    Add meta description and og-image

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,8 @@ import './Footer.css'
 
 const Footer = () => (
     <footer className="page-footer">
-        <p>Teemu T. Tuovinen | <a href="https://nokturno.fi" target="_BLANK">nokturno.fi</a> | 2019–</p>
+        <p>Ruumena on työkalu menetelmällisiin tekstikokeiluihin</p>
+        <p>Teemu T. Tuovinen | <a href="https://nokturno.fi" target="_BLANK">nokturno.fi</a> | 2019</p>
         <p>esimerkkiteksti: ote <a href="https://www.gutenberg.org/ebooks/56496" target="_BLANK">Elias Lönnrotin matkapäiväkirjasta</a></p>
     </footer>
 )
