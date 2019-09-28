cd57205050f10bfc82642e35677503ff38fd7942
Teemu Tuovinen <teetuo@Teemus-MacBook-Pro.local>
2019-09-28 20:44:04 +0300

    Add link to git to footer

--- src/Footer.js
+++ src/Footer.js
@@ -5,8 +5,9 @@ import './Footer.css'
 const Footer = () => (
     <footer className="page-footer">
         <p>Ruumena on työkalu menetelmällisiin tekstikokeiluihin</p>
-        <p>Teemu T. Tuovinen | <a href="https://nokturno.fi" target="_BLANK">nokturno.fi</a> | 2019</p>
-        <p>esimerkkiteksti: ote <a href="https://www.gutenberg.org/ebooks/56496" target="_BLANK">Elias Lönnrotin matkapäiväkirjasta</a></p>
+        <p>Teemu T. Tuovinen | <a href="https://nokturno.fi" target="_blank" rel="noopener noreferrer">nokturno.fi</a> | 2019</p>
+        <p>esimerkkiteksti: ote <a href="https://www.gutenberg.org/ebooks/56496" target="_blank" rel="noopener  noreferrer">Elias Lönnrotin matkapäiväkirjasta</a></p>
+        <p><a href="https://github.com/ttuovinen/ruumena" target="_blank" rel="noopener noreferrer">git</a></p>
     </footer>
 )
 
