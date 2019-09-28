66bfeee57c7b0b44cc8a0c834f620734276521d7
Teemu Tuovinen <teetuo@Teemus-MacBook-Pro.local>
2019-09-28 20:56:16 +0300

    Minor code reformatting in removers

--- src/tools/removers.js
+++ src/tools/removers.js
@@ -1,7 +1,11 @@
 /* From 'seed' input string, replaces every
    'removeN':th word's  characters with ____'s
    starting from 'removeOffset':th word */
-export const removeConstantWords = ({ seed, removeN, removeOffset }) => {
+export const removeConstantWords = ({
+  seed,
+  removeN,
+  removeOffset,
+}) => {
   let curOffset = removeOffset
   return (
     seed
@@ -14,7 +18,8 @@ export const removeConstantWords = ({ seed, removeN, removeOffset }) => {
           return word
         }
         return (((idx % removeN) - ((curOffset - 1) % removeN) === 0) && (idx >= curOffset - 1))
-          ? word.replace(/./g, '_') : word
+          ? word.replace(/./g, '_')
+          : word
       })
       .join(' ')
       .replace(/\n /g, '\n') // remove earlier added spaces
@@ -23,7 +28,10 @@ export const removeConstantWords = ({ seed, removeN, removeOffset }) => {
 
 /* From 'seed' input string, replaces each word's 
    characters with ____'s by 'removePercent' change % */
-export const removeRandomWords = ({ seed, removePercent }) => (
+export const removeRandomWords = ({
+  seed,
+  removePercent,
+}) => (
   seed
     .replace(/\n/g, '\n ') // new word on line break
     .split(' ')
